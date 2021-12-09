e10f7f3815468c389d468a01d8753ca03c7e5512
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-09 12:40:36 -0300

    created: componente SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,10 +1,39 @@
 import React from "react";
+import { Wrapper, Background, InputContainer, ButtonContainer } from './styles';
+
+import Card from '../../components/card';
+import Input from "../../components/Input";
+
+import background from '../../assets/images/background-login.jpg';
+import logoInter from '../../assets/images/Inter-orange.png';
+import Button from "../../components/Button";
+import { Link, useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+
+    const navigate = useNavigate();
+    const handleToSignUp = () => {
+        navigate('/dashboard');
+    }
+
     return (
-        <div>
-            SignUp
-        </div>
+        <Wrapper>
+            <Background image={background} />
+            <Card width="403px">
+                <img src={logoInter} width={172} height={61} alt="logo inter"/>
+                <InputContainer>
+                    <Input placeholder="NOME"/>
+                    <Input placeholder="SOBRENOME"/>
+                    <Input placeholder="EMAIL"/>
+                    <Input placeholder="SENHA" type="password" />
+                    <Input placeholder="CONFIRMAR SENHA" type="password" />
+                </InputContainer>
+                <ButtonContainer>
+                    <Button type="button" onClick={handleToSignUp}>Entrar</Button>
+                    <p>Já tem uma conta? <Link to="/">Entre já!</Link></p>
+                </ButtonContainer>
+            </Card>
+        </Wrapper>
     )
 }
 
