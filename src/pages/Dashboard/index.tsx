34e5fb6030ea3a48b9f1be8814e3b8bae7ecb48a
Vinícius Fernandes <viniciusfernandesmct@gmail.com>
2021-12-09 12:40:04 -0300

    created: componente Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,61 @@
-import React from "react";
+import { BodyContainer, DashboardBackground, InlineContainer, InlineTitle } from './styles'
+
+import Header from '../../components/Header';
+import Card from '../../components/card';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import Statement from './Statement';
 
 const Dashboard = () => {
+    const wallet = 3000;
+    
     return (
-        <div>
-            Dashboard
-        </div>
+        <DashboardBackground>
+            <Header />
+            <BodyContainer>
+                <div>
+                    <Card noShadow width="90%">
+                        <InlineTitle>
+                            <h2 className="h2">Saldo Atual</h2>
+                        </InlineTitle>
+                        <InlineContainer>
+                            <h3 className="wallet">
+                                {wallet.toLocaleString('pt-BR', {style: 'currency', currency:'BRL'})}
+                            </h3>
+                        </InlineContainer>
+                    </Card>
+                    <Card noShadow width="90%">
+                        <InlineTitle>
+                            <h2 className="h2">Receber PIX</h2>
+                        </InlineTitle>
+                        <InlineContainer>
+                            <Input style={{flex: 1}} placeholder="VALOR"/>
+                            <Button>Gerar código</Button>
+                        </InlineContainer>
+
+                        <p className="primary-color">Pix copia e cola</p>
+                        <p className="primary-color">afgf8h651gh8as3ghf654asd8g3222fh</p>
+                    </Card>
+                    <Card noShadow width="90%">
+                        <InlineTitle>
+                            <h2 className="h2">Pagar PIX</h2>
+                        </InlineTitle>
+                        <InlineContainer>
+                            <Input style={{flex: 1}} placeholder="CHAVE PIX"/>
+                            <Button>Pagar PIX</Button>
+                        </InlineContainer>
+                    </Card>
+                </div>
+                <div>
+                    <Card noShadow width="90%">
+                        <InlineTitle>
+                            <h2 className="h2">Extrato da Conta</h2>
+                        </InlineTitle>
+                        <Statement />
+                    </Card>
+                </div>
+            </BodyContainer>
+        </DashboardBackground>
     )
 }
 
